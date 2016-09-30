281e6126beab6c7148c4b818139212a556758406
Mike Solomon <msolomon@thrillist.com>
2016-09-29 21:01:54 -0400

    attach images to jira issues

--- chrome_ext/eventPage.js
+++ chrome_ext/eventPage.js
@@ -9,49 +9,86 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       break;
     case 'create_issue':
       createIssue().then(issue => {
-        addScreenShotToIssue(issue)
-          .then(data => {
+        saveScreenshotToDisk().then(screenshotBlob => {
+          addScreenShotToIssue(issue, screenshotBlob).then(data => {
             getAdditionalIssueInfo(issue).then(additonalIssueInfo => {
               sendChromeNotification(additonalIssueInfo);
             })
           })
-          .catch(data => {
-            console.log(data);
-          })
+        })
       });
       break;
   }
 });
 
-function addScreenShotToIssue(issue) {
+function addScreenShotToIssue(issue, screenshotBlob) {
   return new Promise(function (resolve, reject) {
-    getScreenshot().then(screenshot => {
-      let data = new FormData();
-      data.append('screenshot', screenshot);
+    let data = new FormData();
+    data.append('file', screenshotBlob, 'capture.jpeg');
 
-      let request = {
-        method: 'post',
-        url: issue.self + '/attachments',
-        headers: [
-          {
-            key: 'X-Atlassian-Token',
-            value: 'nocheck'
-          }
-        ],
-        data: data,
-        contentType: false,
-        processData: false
-      }
+    let request = {
+      method: 'post',
+      url: issue.self + '/attachments',
+      headers: [
+        {
+          key: 'X-Atlassian-Token',
+          value: 'nocheck'
+        }
+      ],
+      data: data,
+      contentType: false,
+      cache: false,
+      processData: false
+    }
 
-      sendAjaxRequest(request)
-        .then(response => {
-          return resolve(response);
-        })
-        .catch(err => {
-          return reject(err);
-        })
-    })
+    sendAjaxRequest(request)
+      .then(response => {
+        return resolve(response);
+      })
+      .catch(err => {
+        return reject(err);
+      })
   })
+}
+
+function saveScreenshotToDisk() {
+  return new Promise(function(resolve, reject) {
+    getScreenshot().then(screenshot => {
+      window.webkitRequestFileSystem(window.TEMPORARY, 5*1024*1024, function (fs) {
+        let file_system = fs;
+        file_system.root.getFile('capture.jpeg', {create: true}, function(fileEntry) {
+          fileEntry.createWriter(function(fileWriter) {
+            fileWriter.onwriteend = function(e) {
+              console.log("Write successfully")
+            };
+            fileWriter.onerror = function(e) {
+              console.log("Write error!")
+            };
+            let content = base64ToBinary(screenshot);
+            let blob = new Blob([content]);
+            fileWriter.write(blob);
+
+            return resolve(blob);
+          });
+        });
+      });
+    });
+  });
+
+  function base64ToBinary(imgUrl) {
+    var BASE64_MARKER = ';base64,';
+    var base64Index = imgUrl.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+    var base64 = imgUrl.substring(base64Index);
+    var raw = window.atob(base64);
+    var rawLength = raw.length;
+    var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+    for (var i = 0; i < rawLength; ++i) {
+      array[i] = raw.charCodeAt(i);
+    }
+    return array;
+  }
+
 }
 
 function getScreenshot() {
@@ -223,6 +260,7 @@ function sendAjaxRequest(request) {
       processData: request.processData,
       url: request.url,
       data: request.data,
+      cache: request.cache,
       beforeSend: function(xhr){
         if (request.headers) {
           request.headers.forEach(function(header) {
